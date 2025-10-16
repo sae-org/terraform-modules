e954e1087727092bb96d1531bcf1f22a2cdf782b
Saeeda <saeedabatool39@gmail.com>
2025-10-16 10:14:50 -0400

    added in condition for iam output

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,4 +1,4 @@
 # reference the actual instance_profile resource name ("profile")
 output "iam_profile" {
-  value = aws_iam_instance_profile.profile.name
+  value = try(aws_iam_instance_profile.profile[0].name, null)
 }
