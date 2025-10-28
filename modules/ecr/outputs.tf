d0be35bca1171ad04b7b51d17db4912967f2eadf
Saeeda <saeedabatool39@gmail.com>
2025-10-28 16:28:22 -0400

    fixed iam outputs file

--- modules/ecr/outputs.tf
+++ modules/ecr/outputs.tf
@@ -2,3 +2,12 @@
 output "ecr_repo_name" {
   value = aws_ecr_repository.ecr_repo.name
 }
+
+output "repo_url" {
+  value = aws_ecr_repository.ecr_repo.repository_url
+}
+
+output "image_uri" {
+  description = "Full image URI with tag for ECS deployment."
+  value       = "${aws_ecr_repository.this.repository_url}:${var.image_tag}"
+}
--- modules/ecr/variables.tf
+++ modules/ecr/variables.tf
@@ -2,3 +2,9 @@ variable "proj_prefix" {
   description = "Prefix added to AWS resource names for identification (e.g., 'my-dev', 'sae-org')"
   type        = string
 }
+
+variable "image_tag" {
+  description = "The Docker image tag (e.g., latest, dev, prod, or commit SHA)."
+  type        = string
+  default     = "latest"
+}
