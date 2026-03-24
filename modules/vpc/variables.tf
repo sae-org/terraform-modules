eb7bfd654a582c417a21615ba4a84cfa2288c4b1
Saeeda <saeedabatool39@gmail.com>
2026-03-24 10:41:11 -0400

    enabled dns and hostnames and added in variables

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -16,3 +16,15 @@ variable "vpc_az" {
   description = "List of AWS Availability Zones used for subnet creation"
   type        = list(string)
 }
+
+variable "enable_dns_support" {
+  description = "Enable DNS support in the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "enable_dns_hostnames" {
+  description = "Enable DNS hostnames in the VPC"
+  type        = bool
+  default     = true
+}
--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -2,7 +2,8 @@
 
 resource "aws_vpc" "vpc" {
   cidr_block = var.cidr_block
-
+  enable_dns_support = var.enable_dns_support
+  enable_dns_hostnames = var.enable_dns_hostnames
   tags = {
     Name = "${var.env}-vpc"
   }
