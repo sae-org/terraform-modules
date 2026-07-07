45af0ef09d0496f4182a6dea309931b2c84d35b7
Saeeda <saeedabatool39@gmail.com>
2026-07-07 16:13:29 -0400

    updated

--- modules/r53/r53-private/main.tf
+++ modules/r53/r53-private/main.tf
@@ -11,20 +11,3 @@ resource "aws_route53_zone" "private_hosted_zone" {
   }
 }
 
-resource "aws_route53_record" "site_domains" {
-  for_each = var.private_records
-
-  zone_id = var.create_private_zone ? aws_route53_zone.private_hosted_zone[0].zone_id : data.terraform_remote_state.r53_pri_zone[0].outputs.r53.zone_id
-
-  name = each.value.name
-
-  type = each.value.type
-
-  records = [
-    each.value.value
-  ]
-
-  ttl = try(each.value.ttl, 300)
-
-  allow_overwrite = true
-}
